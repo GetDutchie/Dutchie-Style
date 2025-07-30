250c8a710414108d66fd94b449b9cd933708b13b
Carson Volker <carson.volker@dutchie.com>
2025-07-30 11:33:37 -0700

    try creating a default config

--- lib/Dutchie/Style.rb
+++ lib/Dutchie/Style.rb
@@ -5,6 +5,15 @@ require "Dutchie/Style/version"
 module Dutchie
   module Style
     class Error < StandardError; end
-    # Your code goes here...
+    
+    # Returns the absolute path to the default configuration file
+    def self.config_path
+      File.expand_path("../../../config/default.yml", __FILE__)
+    end
+    
+    # Returns the gem's root directory
+    def self.root
+      File.expand_path("../../../", __FILE__)
+    end
   end
 end
