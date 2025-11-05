ddd512130e2fca0471681938370f49b536717be9
Alex Beeken <alex.beeken@dutchie.com>
2025-11-05 14:57:28 -0800

    some rubocop fixes

--- lib/Dutchie/Style.rb
+++ lib/Dutchie/Style.rb
@@ -5,15 +5,15 @@ require "Dutchie/Style/version"
 module Dutchie
   module Style
     class Error < StandardError; end
-    
+
     # Returns the absolute path to the default configuration file
     def self.config_path
-      File.expand_path("../../../config/default.yml", __FILE__)
+      File.expand_path("../../config/default.yml", __dir__)
     end
-    
+
     # Returns the gem's root directory
     def self.root
-      File.expand_path("../../../", __FILE__)
+      File.expand_path("../..", __dir__)
     end
   end
 end
